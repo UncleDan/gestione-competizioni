5ce9838a7f5408c53e3a7bee7cd4134f197b68ef
Daniele Lolli (UncleDan) <github@uncledan.it>
2026-06-02 17:26:51 +0200

    versioning ubuntu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gestione-competizioni-v1';
+const CACHE_NAME = 'gestione-competizioni-sw-1.0';
 const BASE = '/gestione-competizioni';
 
 // File da mettere in cache all'installazione
